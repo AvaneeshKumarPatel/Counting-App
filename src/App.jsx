290f1b6aae6877fb83a16aef230406c90eafbae6
avnish patel <avnishpatel855@gmail.com>
2024-11-20 12:04:55 +0530

    added reset button

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,25 @@ const [Counter, setCounter] = useState(0);
 
   const InCreaseTheValue = () => {
     setCounter(Counter +1)
-    console.log(Counter);
   }
 
   const  DeCreaseTheValue = () =>{
     setCounter(Counter -1)
-    console.log(Counter);
   }
 
+   const ResetValue = () => {
+    setCounter(0);
+   }
   return (
     <>
-      <h1>Avnish Counter App</h1>
+      <h1>Counting Web App</h1>
 
       <h2>Your Current Value is:  {Counter}</h2> 
        
       <button onClick={InCreaseTheValue}>Increase The Value </button>
       <button onClick={DeCreaseTheValue}>Decrease The Value </button>
+      <button onClick={ResetValue}>Reset </button>
+
 
     </>
   )
